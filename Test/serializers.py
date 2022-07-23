2fbb6589ad731bc50187e65eaac4f8ea0649b5cd
mostaf1elgzeere <your_email_address@example.com>
2022-07-23 20:27:44 +0200

    Registartion

--- Test/serializers.py
+++ Test/serializers.py
@@ -1,3 +1,5 @@
+import email
+from typing_extensions import Required
 from Test.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.serializers import *
@@ -6,11 +8,20 @@ class RegisterSerializer(ModelSerializer):
     class Meta:
         model=User
         data_joined = ReadOnlyField()
+        password=CharField(write_only=True,required=True)
 
         fields = ('id', 'email', 'first_name', 'last_name',
                   'data_joined', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
+    
+    def create(self, validated_data):
+        user=User.objects.create_user(
+        email=validated_data['email'],
+        password=validated_data['password']
+        )
+        return user
+        
+        
+         
 class NewLoginSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
