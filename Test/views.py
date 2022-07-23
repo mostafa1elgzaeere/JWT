7423fb1d818f31cc3fa6dcb28eb3da6e4fe9cfa1
mostaf1elgzeere <your_email_address@example.com>
2022-07-23 18:13:54 +0200

    registertion

--- Test/views.py
+++ Test/views.py
@@ -4,13 +4,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 
-class RegisterView(APIView):
-   def post(self, request):
-        user = request.data
-        serializer = RegisterSerializer(data=user)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class RegisterView(CreateAPIView):
+    serializer_class=RegisterSerializer
+#    def post(self, request):
+#         user = request.data
+#         serializer = RegisterSerializer(data=user)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
     
 class NewLoginView(TokenObtainPairView):
     serializer_class=NewLoginSerializer
